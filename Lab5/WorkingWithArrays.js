60b0c3b45bc740a53eca2b583a7a86457944716c
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-11-08 21:15:39 -0500

    complete labs

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -41,10 +41,50 @@ export default function WorkingWithArrays(app) {
     todo.title = title;
     res.json(todos);
   };
+  const postNewTodo = (req, res) => {
+    const newTodo = {
+      id: new Date().getTime(),
+      title: "New Task",
+      completed: false,
+      ...req.body,
+    };
+    todos.push(newTodo);
+    res.json(newTodo);
+  };
+  const deleteTodo = (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+      res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+      return;
+    }
+
+    todos.splice(todoIndex, 1);
+    res.sendStatus(200);
+  };
+  const updateTodo = (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+      res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
+      return;
+    }
+
+    todos = todos.map((t) => {
+      if (t.id === parseInt(id)) {
+        return { ...t, ...req.body };
+      }
+      return t;
+    });
+    res.sendStatus(200);
+  };
+  app.put("/lab5/todos/:id", updateTodo);
+  app.delete("/lab5/todos/:id", deleteTodo);
   app.get("/lab5/todos/:id/title/:title", updateTodoTitle);
   app.get("/lab5/todos/:id/delete", removeTodo);
 
   app.get("/lab5/todos/:id", getTodoById);
   app.get("/lab5/todos", getTodos);
   app.get("/lab5/todos/create", createNewTodo);
+  app.post("/lab5/todos", postNewTodo);
 }
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
@@ -6,11 +7,12 @@ import QueryParameters from "./Lab5/QueryParameter.js";
 import WorkingWithArrays from "./Lab5/WorkingWithArrays.js";
 import WorkingWithModules from "./Lab5/WorkingWithModules.js";
 import WorkingWithObjects from "./Lab5/WorkingWithObject.js";
+
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 4000;
 
 app.use(express.json()); // if you parse JSON bodies
-
 Lab5(app);
 Hello(app);
 PathParameters(app);
