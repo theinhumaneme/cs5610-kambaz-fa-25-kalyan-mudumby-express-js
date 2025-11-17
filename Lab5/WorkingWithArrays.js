c95cb30f0d902f81567a2309d35c4302ced19d77
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-11-16 23:47:04 -0500

    final fixup

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -83,8 +83,8 @@ export default function WorkingWithArrays(app) {
   app.get("/lab5/todos/:id/title/:title", updateTodoTitle);
   app.get("/lab5/todos/:id/delete", removeTodo);
 
+  app.get("/lab5/todos/create", createNewTodo);
   app.get("/lab5/todos/:id", getTodoById);
   app.get("/lab5/todos", getTodos);
-  app.get("/lab5/todos/create", createNewTodo);
   app.post("/lab5/todos", postNewTodo);
 }
